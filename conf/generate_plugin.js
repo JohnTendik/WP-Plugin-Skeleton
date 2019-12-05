e392d3010fe7eae915752b298a3ae97f9cd1acea
unknown <johntendik@hotmail.com>
2019-12-04 19:42:46 -0500

    update generator to actually working state

--- conf/generate_plugin.js
+++ conf/generate_plugin.js
@@ -15,51 +15,38 @@ const pluginConst = pluginPackage.const;
 const traverseFolders = async(curPath) => {
   // Get all files in the current folder
   const files = fs.readdirSync(curPath);
-
-  // Filter files that need renaming
-  const renameItems = files.filter((file) => file.includes('@plugin-name@'));
-  await renameFiles(curPath, renameItems);
-
-  // Filter php files that need renaming within the files
-  const renameInnerFiles = files.filter((file) => file.includes('.php'));
-  // Rename everything within the files
-  await innerRename(curPath, renameInnerFiles);
+  files.reduce(async(prevPromise, filePath) => {
+    if (filePath.includes('@plugin-name@')) {
+      await renameFile(curPath, filePath);
+    }
+    if (filePath.includes('.php')) {
+      await innerRename(curPath, filePath)
+    }
+    if (fs.statSync(path.join(curPath, filePath)) && fs.statSync(path.join(curPath, filePath)).isDirectory() && filePath !== '.git') {
+      traverseFolders(path.join(curPath, filePath))
+    }
+    Promise.resolve();
+  }, Promise.resolve());
+  
 }
 
-const renameFiles = (curPath, files) => {
-  return new Promise((resolve) => {
-    if (files.length) {
-      files.forEach((fileName, indx) => {
-        const newName = fileName.replace('@plugin-name@', pluginName);
-        fs.renameSync(path.join(curPath, fileName), path.join(curPath, newName));
-        if (indx == files.length - 1) {
-          resolve();
-        }
-      });
-    } else {
-      resolve();
-    }
+const renameFile = (curPath, filePath) => {
+  return new Promise((res) => {
+    const newName = filePath.replace('@plugin-name@', pluginName);
+    fs.renameSync(path.join(curPath, filePath), path.join(curPath, newName));
+    res();
   });
 }
 
-const innerRename = (curPath, files) => {
-  return new Promise((resolve) => {
-    if (files.length) {
-      files.forEach((fileName, indx) => {
-        const filePath = path.join(curPath, fileName);
-        const fileData = fs.readFileSync(filePath, 'utf8');
-        var result = fileData.replace(/@plugin-name@/g, pluginName);
-        result = result.replace(/@plugin-slug@/g, pluginSlug);
-        result = result.replace(/@plugin-const@/g, pluginConst);
-        fs.writeFileSync(filePath, result, 'utf8');
-        
-        if (indx == files.length - 1) {
-          resolve();
-        }
-      });
-    } else {
-      resolve();
-    }
+const innerRename = (curPath, filePathArg) => {
+  return new Promise((res) => {
+    const filePath = path.join(curPath, filePathArg);
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    var result = fileData.replace(/@plugin-name@/g, pluginName);
+    result = result.replace(/@plugin-slug@/g, pluginSlug);
+    result = result.replace(/@plugin-const@/g, pluginConst);
+    fs.writeFileSync(filePath, result, 'utf8');
+    res();
   });
 }
 
