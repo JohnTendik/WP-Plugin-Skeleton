4e217358b85d8e70149cc89eaa69a0d8ac949ca5
unknown <johntendik@hotmail.com>
2020-01-11 12:56:46 -0500

    quick update

--- conf/generate_plugin.js
+++ conf/generate_plugin.js
@@ -6,7 +6,9 @@ const pluginPackage = require(path.join(rootPath, 'package.json'));
 
 const pluginName = pluginPackage.name;
 const pluginSlug = pluginPackage.slug;
+const pluginNamePretty = pluginPackage.prettyName;
 const pluginConst = pluginPackage.const;
+const pluginClassName = pluginPackage.className;
 
 /**
 * Recursively traverse through the plugin folders to rename necessary areas
@@ -16,11 +18,11 @@ const traverseFolders = async(curPath) => {
   // Get all files in the current folder
   const files = fs.readdirSync(curPath);
   files.reduce(async(prevPromise, filePath) => {
-    if (filePath.includes('@plugin-name@')) {
-      await renameFile(curPath, filePath);
-    }
     if (filePath.includes('.php')) {
       await innerRename(curPath, filePath)
+    }
+    if (filePath.includes('@plugin-name@')) {
+      await renameFile(curPath, filePath);
     }
     if (fs.statSync(path.join(curPath, filePath)) && fs.statSync(path.join(curPath, filePath)).isDirectory() && filePath !== '.git') {
       traverseFolders(path.join(curPath, filePath))
@@ -44,7 +46,9 @@ const innerRename = (curPath, filePathArg) => {
     const fileData = fs.readFileSync(filePath, 'utf8');
     var result = fileData.replace(/@plugin-name@/g, pluginName);
     result = result.replace(/@plugin-slug@/g, pluginSlug);
+    result = result.replace(/@plugin-name-pretty@/g, pluginNamePretty);
     result = result.replace(/@plugin-const@/g, pluginConst);
+    result = result.replace(/@plugin-className@/g, pluginClassName);
     fs.writeFileSync(filePath, result, 'utf8');
     res();
   });
